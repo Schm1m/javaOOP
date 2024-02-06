6077811ec9f8f61ece1cfcfda69d592319efec9f
Schm1m <tim@schm1m.com>
2024-02-06 15:32:12 +0000

    v0.9

---
+++ tasks/task-3/task.java
@@ -0,0 +1,65 @@
+public class task {
+    public static void main(String[] args) {
+        Point p = new Point();
+        Circle c = new Circle(p);
+        System.out.println("part 1:");
+        p.print();
+        c.print();
+        p.setX(1.1);
+        p.setY(2.2);
+        c.set_centre(p);
+        c.set_radius(3.3);
+        System.out.println("part 2:");
+        p.print(false);
+        System.out.print(" == ()" + p.getX() + "," + p.getY() + ")\n");
+        c.print();
+        p.move(1.0, 1.0);
+        c.move(2.0, 2.0);
+        System.out.println("part 3:");
+        p.print();
+        c.print();
+    }
+}
+
+class Point {
+    double x;
+    double y;
+
+    Point() {
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+
+    void print(boolean endl) {
+        if (endl = true) {
+            System.out.println("(" + x + "," + y + ")");
+        } else {
+            this.print();
+        }
+    }
+
+    void print() {
+        System.out.print("(" + x + "," + y + ")");
+    }
+
+    void setX(double input) {
+        this.x = input;
+    }
+
+    void setY(double input) {
+        this.y = input;
+    }
+
+    double getX() {
+        return this.x;
+    }
+
+    double getY() {
+        return this.y;
+    }
+}
+
+// define class circle here
+class Circle {
+    Point centre;
+}
